b5a1bead24a439252c94ce8fa4ce821fbbb18e3e
Richard Willis <willis.rh@gmail.com>
2020-08-04 13:11:40 +0100

    Update publishing workflows

--- scripts/generate-php.sh
+++ scripts/generate-php.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+PACKAGE_VERSION=${PACKAGE_VERSION:-"0.0.0"}
+
 PHP_POST_PROCESS_FILE="$(npm bin)/prettier --write --config ./.prettierrc.php.json" \
 $(npm bin)/openapi-generator generate \
   -i schema/mailinabox.yml \
   -g php \
+  -t templates/php \
   -o clients/php \
   --additional-properties=variableNamingConvention=camelCase \
   --enable-post-process-file \
   --package-name=MailInABoxAPI \
   --invoker-package='MailInABoxAPI\\Client' \
-  --generate-alias-as-model
+  --generate-alias-as-model \
+  --git-user-id=badsyntax \
+  --git-repo-id=mailinabox-api-php \
+  --git-host=github.com \
+  --artifact-id=badsyntax/mailinabox-api \
+  --artifact-version="$PACKAGE_VERSION" \

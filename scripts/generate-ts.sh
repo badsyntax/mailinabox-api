2c58e36f2cc5afd5eb8927e580b0fbefef12a193
Richard Willis <willis.rh@gmail.com>
2020-08-04 14:34:08 +0100

    Don't add package-lock.json to typescript package

--- scripts/generate-ts.sh
+++ scripts/generate-ts.sh
@@ -15,6 +15,3 @@ $(npm bin)/openapi-generator generate \
   --git-user-id=badsyntax \
   --git-repo-id=mailinabox-api-ts \
   --git-host=github.com
-
-# generate a package-lock.json
-npm i --prefix "$CLIENT_PATH"
